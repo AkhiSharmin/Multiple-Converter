acc633ec333ee1f5469e999cd026a823f463ae4f
AkhiSharmin <akhisharmin100@gmail.com>
2024-03-05 20:45:40 +0600

    some of update

--- app.js
+++ app.js
@@ -192,8 +192,11 @@ let lastRightSelectedValue = '';
 
 function main() {
     const categorySelect = document.getElementById('category-select');
+    const leftInput = document.getElementById('left-inp');
+    const rightInput = document.getElementById('right-inp');
     const leftSelect = document.getElementById('left-select');
     const rightSelect = document.getElementById('right-select');
+    const formulaText = document.getElementById('formula-text');
 
     const coveterKeys = Object.keys(converter).sort();
     removeAllChild(categorySelect);
@@ -203,6 +206,18 @@ function main() {
 
     //set default category units
     updateCategoryChanges(categorySelect, leftSelect, rightSelect);
+
+
+    const converterName = categorySelect.value;
+    const variants = converter[converterName].variants;
+    const variantKey = `${leftSelect.value}:${rightSelect.value}`;
+    const variant = variants[variantKey];
+    formulaText.innerText = variant.formula
+
+
+    leftInput.value = 1
+    rightInput.value = variant.calculation(1)
+
 
     categorySelect.addEventListener('change', function () {
         updateCategoryChanges(categorySelect, leftSelect, rightSelect);
@@ -288,3 +303,7 @@ function updateCategoryChanges(categorySelect, leftSelect, rightSelect) {
 
     lastRightSelectedValue = rightSelect.value;
 };
+
+function calculateValue() {
+
+}
